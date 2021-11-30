2d79b7bbd742b523804412153c715b60cdb6ed70
RoviLuca <Roviluca@users.noreply.github.com>
2021-11-30 23:23:41 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import prefect
-from prefect import task
+from prefect import task, Flow
 
 @task
 def say_hello():
     logger = prefect.context.get("logger")
     logger.info("Hello, JD!!")
+with Flow("hello-flow-tutorial") as flow:
+    say_hello()
